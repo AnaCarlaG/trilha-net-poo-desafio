9cd503ca63b0145d4cf4b802d4c60f8c88e2cac9
ana.gomes <ana.gomes@tceal.tc.br>
2025-06-17 20:24:36 -0300

    Implementando as funcionalidades que faltaram usando herança e polimorfismo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123", imei: "12", modelo: "A71", memoria: 200);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone iphone = new Iphone(numero: "123", imei: "12", modelo: "X", memoria: 200);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
